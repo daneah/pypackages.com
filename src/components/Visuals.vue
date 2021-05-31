1714889356cb30ffb55812fce3321c03da4406b2
Dane Hillard <github@danehillard.com>
2021-05-31 14:57:22 -0400

    Add more info/structure  to the landing page

---
+++ src/components/Visuals.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="visuals" role="img" aria-label="Packaging Python code and publishing it to be shared">
+    🐍&nbsp;&nbsp;📦&nbsp;&nbsp;&nbsp;⬆️
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Visuals',
+}
+</script>
+
+<style lang="scss" scoped>
+.visuals {
+  font-size: var(--font-size-huge);
+  margin: var(--spacing-roomy) 0;
+  text-align: center;
+}
+</style>
